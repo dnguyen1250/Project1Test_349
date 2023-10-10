c20d36c26eb916c92b9ba982e331be2d442a696c
dnguyen1250 <danielvunguyen233@gmail.com>
2023-10-09 19:05:52 -0700

    added 2 navbars, 1 for logged in, 1 for logged out

--- navbar_loggedout_script.js
+++ navbar_loggedout_script.js
@@ -5,17 +5,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const xhr = new XMLHttpRequest();
 
   // Configure it: GET-request for the URL /navbar.html
-  xhr.open('GET', 'navbar.html', true);
+  xhr.open('GET', 'navbar_loggedout.html', true);
 
   // Send the request over the network
   xhr.send();
 
   // This will be called after the response is received
   xhr.onload = function() {
-      if (xhr.status != 200) { // analyze HTTP response
-          alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-      } else { // show the result
-          header.innerHTML = xhr.responseText;
-      }
+    if (xhr.status != 200) { // analyze HTTP response
+        alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+        header.innerHTML = xhr.responseText;
+    }
   };
 });
+
